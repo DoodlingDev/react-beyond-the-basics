29ed08e53543732f012e7585a989e3462c76ed5e
DoodlingDev <aji.slater@gmail.com>
2019-04-07 20:25:01 -0500

    Adds NewTootInput

--- src/NewToot.js
+++ src/NewToot.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import NewTootInput from "./NewTootInput";
 
 export default class NewToot extends Component {
   render() {
     return (
       <form className="new-toot__form">
-        <input type="text" className="new-toot__input" maxlength="40" />
+        <NewTootInput />
 
         <button className="new-toot__button">Toot</button>
       </form>
---
+++ src/NewTootInput.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function NewTootInput() {
+  return (
+    <input type="text" className="new-toot__input" maxlength="40" />
+  );
+}
