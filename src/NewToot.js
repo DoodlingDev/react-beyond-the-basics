f8c4a8ceeae9898cdff6f68e915e5afebd40adf1
DoodlingDev <aji.slater@gmail.com>
2019-04-10 16:57:52 -0500

    Add constructor and state to NewToot
    NewToot is going to be where we keep the state for our tiny little form.
    We want to initialize the value of `inputValue` as soon as the Component
    is mounted into the page, so we'll use the constructor to accomplish it.
    
    Because we want *this component*, the one we're working in right now, to
    have the state, we're going to set the state parameter on `this` by
    using `this.state =`.
    
    `this` can be a tricky concept in JavaScript, so we'll try and keep it
    simple by addressing it when we need to use it here, and not get too
    deep in the weeds just yet.

--- src/NewToot.js
+++ src/NewToot.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import NewTootInput from "./NewTootInput";
 
 export default class NewToot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    };
+  }
+
   render() {
     return (
       <div className="new-toot__form">
